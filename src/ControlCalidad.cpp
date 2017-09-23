4bbc423ab96e3d8fb406b36445b1acc9e12e3aa2
Edgardo Marchi <edgardo.marchi@gmail.com>
2017-09-23 12:23:48 -0300

    Se agrega control de calidad no terminado

---
+++ src/ControlCalidad.cpp
@@ -0,0 +1,89 @@
+#include <string>
+
+#include "message.h"
+#include "parsimu.h"
+//#include "real.h"
+//#include "tuple_value.h"
+
+#include "ControlCalidad.h"
+
+using namespace std;
+
+
+ControlCalidad::ControlCalidad(const string &name) :
+	Atomic(name),
+	prod_i(addInputPort("prod_i")),
+	queryClient_i(addInputPort("queryInventory_o")), 
+	queryInventory_o(addOutputPort("queryInventory_o")), 
+	prod_o(addOutputPort("prod_o"))
+{
+}
+
+
+Model &ControlCalidad::initFunction()
+{
+	holdIn(AtomicState::passive, VTime::Inf);
+	state = State::WAITING;
+	return *this;
+}
+
+
+Model &ControlCalidad::externalFunction(const ExternalMessage &msg)
+{
+	if (msg.port() == prod_i)
+	{
+		Tuple<double> products = Tuple<double>::from_value(msg.value());
+		std::cout<<products.size();
+	}
+	
+	if (msg.port() == queryClient_i)
+	{
+		if (state == State::WAITING)	// Redundant
+		{
+			numClientQuery = (Real::from_value(msg.value())).value();
+			state = State::QUERY;
+		}
+	}
+	else
+		// No time Advance in this module
+		holdIn(AtomicState::passive, VTime::Inf);
+	
+	return *this;
+}
+
+
+Model &ControlCalidad::internalFunction(const InternalMessage &)
+{
+	switch(state)
+	{
+		case State::QUERY:
+			holdIn(AtomicState::passive, VTime::Inf);
+			state = State::INV_WAIT;
+			break;
+		case State::CHECK:
+			if (numPassProd <= numClientQuery)
+				state = State::QUERY;
+			else
+				state = State::WAITING;
+			break;
+		default:	// Shouldnt enter here
+			holdIn(AtomicState::passive, VTime::Inf);
+			state = State::WAITING;
+			break;
+	}
+	return *this ;
+}
+
+
+Model &ControlCalidad::outputFunction(const CollectMessage &msg)
+{
+	switch(state)
+	{
+		case State::QUERY:
+			sendOutput(msg.time(), queryInventory_o, numClientQuery);
+			break;
+		default:
+			break;
+	}
+	return *this ;
+}
---
+++ src/ControlCalidad.h
@@ -0,0 +1,34 @@
+#ifndef _CONTROLINVENTARIO_H_
+#define _CONTROLINVENTARIO_H_
+
+#include "atomic.h"
+#include "VTime.h"
+
+
+#define ATOMIC_MODEL_NAME "ControlCalidad"
+
+
+class ControlCalidad : public Atomic {
+  public:
+    ControlCalidad(const string &name = ATOMIC_MODEL_NAME );
+    virtual string className() const {return ATOMIC_MODEL_NAME;}
+    enum class State {WAITING, CHECK, QUERY, INV_WAIT};
+
+  protected:
+    Model &initFunction();
+    Model &externalFunction( const ExternalMessage & );
+    Model &internalFunction( const InternalMessage & );
+    Model &outputFunction( const CollectMessage & );
+
+  private:
+    const Port &prod_i;
+    const Port &queryClient_i;
+    Port &queryInventory_o;
+    Port &prod_o;
+	
+	double numClientQuery{0};
+	double numPassProd{0};
+    State state = {State::WAITING};
+};
+
+#endif
